570ff4aa9264e89ed2b3d751bfe650f5bbe4540d
unknown <felixlopezarm1@gmail.com>
2019-05-11 19:34:37 -0400

    :tada:initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongoose = require('mongoose');
+
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+// routes
+app.use('/', (req,res) => {
+  res.json({
+    message:'hello World'
+  })
+});
+
+if(process.env.NODE_ENV === 'production'){
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*' , (req, res) => {
+    res.sendFile(path.resolve(__dirname,'client', 'build' , 'index.html'))
+  })
+}
+
+module.exports = app;
